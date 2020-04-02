0dcf1828555846f9a71aadaa143492babacad794
Graeme Diack <graemediack@hotmail.com>
2020-04-02 16:09:00 +0000

    updates: tomcat and java v8 and added  variable

--- deploy_erddap.sh
+++ deploy_erddap.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
+SCRIPT_HOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 #update system
 apt update
 apt upgrade
 #install JDK
-apt install default-jdk
+apt install openjdk-8-jdk
 #install unzip
 apt install unzip
 ###
 #Download files (note: tmp dir will clear out on reboots, optionally download to homedir and add step to delete at the end)
 cd /tmp
-wget https://www.mirrorservice.org/sites/ftp.apache.org/tomcat/tomcat-9/v9.0.33/bin/apache-tomcat-9.0.33.tar.gz
+wget https://www.mirrorservice.org/sites/ftp.apache.org/tomcat/tomcat-8/v8.5.53/bin/apache-tomcat-8.5.53.tar.gz
 wget https://github.com/BobSimons/erddap/releases/download/v2.02/erddapContent.zip
 wget https://github.com/BobSimons/erddap/releases/download/v2.02/erddap.war
 ###
@@ -30,7 +31,7 @@ chmod -R g+r /opt/tomcat/conf
 chmod g+x /opt/tomcat/conf
 chown -R tomcat /opt/tomcat/webapps/ /opt/tomcat/work/ /opt/tomcat/temp/ /opt/tomcat/logs/
 #create systemd service file (pull from github repo)
-cp ~/erddap_config/files/tomcat.service /etc/systemd/system/tomcat.service
+cp $SCRIPT_HOME/files/tomcat.service /etc/systemd/system/tomcat.service
 #May require editing of JAVA_HOME and Memory Allocation
 #JAVA_HOME = sudo update-java-alternatives -l
 #sudo nano /etc/systemd/system/tomcat.service
@@ -46,9 +47,9 @@ systemctl daemon-reload
 systemctl enable tomcat
 #configure tomcat user for management interfaces
 #copy files from git repo
-cp ~/erddap_config/files/tomcat-users.xml /opt/tomcat/conf/tomcat-users.xml
-cp ~/erddap_config/files/mgr_context.xml /opt/tomcat/webapps/manager/META-INF/context.xml
-cp ~/erddap_config/files/hmgr_context.xml /opt/tomcat/webapps/host-manager/META-INF/context.xml
+cp $SCRIPT_HOME/files/tomcat-users.xml /opt/tomcat/conf/tomcat-users.xml
+cp $SCRIPT_HOME/files/mgr_context.xml /opt/tomcat/webapps/manager/META-INF/context.xml
+cp $SCRIPT_HOME/files/hmgr_context.xml /opt/tomcat/webapps/host-manager/META-INF/context.xml
 #edit files if required (recommend edit tomcat-users.xml to more appropriate password)
 #sudo nano /opt/tomcat/conf/tomcat-users.xml
 #sudo nano /opt/tomcat/webapps/manager/META-INF/context.xml
@@ -62,7 +63,7 @@ read -n 1 -s -r -p "install and configure apache..."
 #Install apache2 (TODO: is this required?)
 apt install apache2
 #apply changes to apache2.conf as per ERDDAP instructions
-cp ~/erddap_config/files/apache2.conf /etc/apache2/apache2.conf
+cp $SCRIPT_HOME/files/apache2.conf /etc/apache2/apache2.conf
 #sudo nano /etc/apache2/apache2.conf
 ###
 echo "Finished - Install and configure apache"
@@ -76,9 +77,9 @@ chown -R tomcat:tomcat /opt/tomcat/content
 #create bigParentDirectory (location optional, but must be represented in setup.xml
 mkdir /home/erddap
 chown -R tomcat:tomcat /home/erddap
-cp ~/erddap_config/files/setup.xml /opt/tomcat/content/erddap/setup.xml
-cp ~/erddap_config/files/server.xml /opt/tomcat/conf/server.xml
-cp ~/erddap_config/files/context.xml /opt/tomcat/conf/context.xml
+cp $SCRIPT_HOME/files/setup.xml /opt/tomcat/content/erddap/setup.xml
+cp $SCRIPT_HOME/files/server.xml /opt/tomcat/conf/server.xml
+cp $SCRIPT_HOME/files/context.xml /opt/tomcat/conf/context.xml
 #sudo nano /opt/tomcat/content/erddap/setup.xml - hostname must be updated
 #sudo nano /opt/tomcat/conf/server.xml
 #sudo nano /opt/tomcat/conf/context.xml
